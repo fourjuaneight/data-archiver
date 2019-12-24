345de3618747d8502f26e785f560907ef699c3a9
Juan Villela <juan@villela.co>
2019-12-24 14:32:05 -0500

    Time data uploader optimizations.

--- timeUploader.js
+++ timeUploader.js
@@ -1,14 +1,17 @@
-#!/usr/bin / env node
-
-const { resolve } = require('path');
+const { readFileSync } = require('fs');
+const { join, resolve } = require('path');
 const axios = require('axios');
 const dotenv = require('dotenv').config({
   path: resolve(process.cwd(), '.env'),
 });
 
-axios({
-  data: {
-    query: `
+const timeData = JSON.parse(readFileSync(join(__dirname, 'time.json'), 'utf8'));
+
+timeData.forEach(
+  obj =>
+    axios({
+      data: {
+        query: `
             mutation TimeMutation {
               insert_time(objects: {
                 created_at: "${obj.Date}T${obj.Start}",
@@ -25,13 +28,14 @@ axios({
               }
             }
           `,
-  },
-  headers: {
-    'Content-Type': 'application/json',
-    'X-Hasura-Admin-Secret': dotenv.parsed.EREBOR_KEY,
-  },
-  method: 'POST',
-  url: dotenv.parsed.EREBOR_ENDPOINT,
-})
-  .then(result => console.info(result.data)) // eslint-disable-line
-  .catch(error => console.error(error));
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Hasura-Admin-Secret': dotenv.parsed.EREBOR_KEY,
+      },
+      method: 'POST',
+      url: dotenv.parsed.EREBOR_ENDPOINT,
+    })
+      .then(result => console.info(result.data)) // eslint-disable-line
+      .catch(error => console.error(error)) // eslint-disable-line
+);
