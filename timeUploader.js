5c3b5cf43ef949c60efa985598a0d4dcdcb15ff5
Juan Villela <juan@villela.co>
2020-01-02 15:44:30 -0500

    Time uploader fixes
    Added a setTimeout to avoid network interruption errors.

--- timeUploader.js
+++ timeUploader.js
@@ -1,41 +1,47 @@
 const { readFileSync } = require('fs');
-const { join, resolve } = require('path');
+const { resolve } = require('path');
 const axios = require('axios');
 const dotenv = require('dotenv').config({
   path: resolve(process.cwd(), '.env'),
 });
 
-const timeData = JSON.parse(readFileSync(join(__dirname, 'time.json'), 'utf8'));
+const timeData = JSON.parse(
+  readFileSync(resolve(__dirname, 'december.json'), 'utf8')
+);
 
-timeData.forEach(
-  obj =>
-    axios({
-      data: {
-        query: `
-            mutation TimeMutation {
-              insert_time(objects: {
-                created_at: "${obj.Date}T${obj.Start}",
-                updated_at: "${obj.Date}T${obj.Finish}",
-                group: "${obj.Group}",
-                task: "${obj.Task}",
-                start: "${obj.Date}T${obj.Start}",
-                finish: "${obj.Date}T${obj.Finish}",
-                duration: "${obj.Duration}"
-              }) {
-                returning {
-                  task
+timeData.forEach(obj => {
+  setTimeout(
+    () =>
+      axios({
+        data: {
+          query: `
+              mutation TimeMutation {
+                insert_time(objects: {
+                  group: "${obj.group}",
+                  task: "${obj.task}",
+                  start: "${obj.start}",
+                  finish: "${obj.finish}",
+                  duration: "${obj.duration}"
+                  created_at: "${obj.created_at}",
+                  updated_at: "${obj.updated_at}",
+                }) {
+                  returning {
+                    task
+                    created_at
+                  }
                 }
               }
-            }
-          `,
-      },
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Hasura-Admin-Secret': dotenv.parsed.EREBOR_KEY,
-      },
-      method: 'POST',
-      url: dotenv.parsed.EREBOR_ENDPOINT,
-    })
-      .then(result => console.info(result.data)) // eslint-disable-line
-      .catch(error => console.error(error)) // eslint-disable-line
-);
+            `,
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Hasura-Admin-Secret': dotenv.parsed.EREBOR_KEY,
+        },
+        method: 'POST',
+        url: dotenv.parsed.EREBOR_ENDPOINT,
+      })
+        .then(result => console.log(result.data))
+        .catch(error => console.error(error)),
+    1500
+  );
+});
